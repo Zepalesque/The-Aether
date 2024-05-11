fd5c8d5d4e8b9c5d555604d439be5a2dcf23de60
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-05-11 12:41:26 -0400

    fix: Scale for baby moa

--- src/main/java/com/aetherteam/aether/client/renderer/geo/MoaGeoRenderer.java
+++ src/main/java/com/aetherteam/aether/client/renderer/geo/MoaGeoRenderer.java
@@ -5,11 +5,15 @@ import com.aetherteam.aether.client.AetherClient;
 import com.aetherteam.aether.client.renderer.entity.MoaRenderer;
 import com.aetherteam.aether.client.renderer.geo.layers.MoaEmissiveGeoLayer;
 import com.aetherteam.aether.entity.passive.Moa;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.core.molang.MolangParser;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -20,17 +24,21 @@ public class MoaGeoRenderer extends GeoEntityRenderer<Moa> {
     }
 
     @Override
+    public void preRender(PoseStack poseStack, Moa moa, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        float moaScale = moa.isBaby() ? 0.5F : 0.9F;
+        poseStack.scale(moaScale, moaScale, moaScale);
+        super.preRender(poseStack, moa, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+
+    @Override
     public @NotNull ResourceLocation getTextureLocation(@NotNull Moa moa) {
         return MoaRenderer.location(moa);
     }
 
     public static class Model extends AetherGeoModel<Moa> {
 
-        private static final ResourceLocation FALLBACK = new ResourceLocation(Aether.MODID, "textures/entity/mobs/moa/blue_moa.png");
         private static final ResourceLocation GEO_LOCATION = new ResourceLocation(Aether.MODID, "geo/moa.geo.json");
         private static final ResourceLocation ANIM_LOCATION = new ResourceLocation(Aether.MODID, "animations/moa.animation.json");
-
-        private float limbSwing, limbSwingAmount;
 
         @Override
         public ResourceLocation getModelResource(Moa moa) {
@@ -48,8 +56,8 @@ public class MoaGeoRenderer extends GeoEntityRenderer<Moa> {
         }
 
         @Override
-        public void applyMolangQueries(Moa animatable, double animTime) {
-            super.applyMolangQueries(animatable, animTime);
+        public void applyMolangQueries(Moa moa, double animTime) {
+            super.applyMolangQueries(moa, animTime);
             MolangParser molangParser = MolangParser.INSTANCE;
             Minecraft mc = Minecraft.getInstance();
 
@@ -57,12 +65,12 @@ public class MoaGeoRenderer extends GeoEntityRenderer<Moa> {
             molangParser.setMemoizedValue(AetherClient.LIMB_MOVEMENT, () -> {
                 float limbSwingAmount = 0;
                 float limbSwing = 0;
-                boolean shouldSit = animatable.isPassenger() && (animatable.getVehicle() != null && animatable.getVehicle().shouldRiderSit());
-                if (!shouldSit && animatable.isAlive()) {
-                    limbSwingAmount = animatable.walkAnimation.speed(mc.getPartialTick());
-                    limbSwing = animatable.walkAnimation.position(mc.getPartialTick());
+                boolean shouldSit = moa.isPassenger() && (moa.getVehicle() != null && moa.getVehicle().shouldRiderSit());
+                if (!shouldSit && moa.isAlive()) {
+                    limbSwingAmount = moa.walkAnimation.speed(mc.getPartialTick());
+                    limbSwing = moa.walkAnimation.position(mc.getPartialTick());
 
-                    if (animatable.isBaby())
+                    if (moa.isBaby())
                         limbSwing *= 3f;
 
                     if (limbSwingAmount > 1f)
@@ -71,8 +79,8 @@ public class MoaGeoRenderer extends GeoEntityRenderer<Moa> {
                 return Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount * Mth.RAD_TO_DEG;
             });
 
-            molangParser.setMemoizedValue(AetherClient.HEAD_X_ROT, () -> Mth.lerp(mc.getPartialTick(), animatable.xRotO, animatable.getXRot()));
-            molangParser.setMemoizedValue(AetherClient.HEAD_Y_ROT, () -> this.calculateHeadY(animatable, mc.getPartialTick()));
+            molangParser.setMemoizedValue(AetherClient.HEAD_X_ROT, () -> Mth.lerp(mc.getPartialTick(), moa.xRotO, moa.getXRot()));
+            molangParser.setMemoizedValue(AetherClient.HEAD_Y_ROT, () -> this.calculateHeadY(moa, mc.getPartialTick()));
 
         }
 
